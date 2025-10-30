a7fad8b01ed7253752b949fdc1b5055974db5dad
stasssercheff <stassserchef@gmail.com>
2025-10-30 11:58:46 +0700

    Update home URL in goHome function

--- kitchen/kitchen/script.js
+++ kitchen/kitchen/script.js
@@ -4,7 +4,7 @@
 
 // На главную
 function goHome() {
-    location.href = "http://stasssercheff.github.io/shbb/";
+    location.href = "http://stasssercheff.github.io/shbb25/";
 }
 
 // На уровень выше (одну папку вверх)
