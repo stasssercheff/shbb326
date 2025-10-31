04383a1f3efed63ea084a9dba756d1e95ec9cb16
stasssercheff <stassserchef@gmail.com>
2025-10-31 09:26:24 +0700

    Update script.js

--- kitchen/pastry/checklists/given/script.js
+++ kitchen/pastry/checklists/given/script.js
@@ -164,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
       while (start < text.length) {
         const chunk = text.slice(start, start + 4000);
         await sendMessage(chunk);
-        await sendEmail(chunk);
+       // await sendEmail(chunk); //
         start += 4000;
       }
     };
