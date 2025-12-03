3f0dcbe8428f87db6a2e777855712cb1ceca96ee
stasssercheff <stassserchef@gmail.com>
2025-12-03 08:39:17 +0700

    Update script.js

--- kitchen/kitchen/checklist/script.js
+++ kitchen/kitchen/checklist/script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // На главную
 function goHome() {
-    location.href = "http://stasssercheff.github.io/shbb25/";
+    location.href = "http://stasssercheff.github.io/shbb125/";
 }
 
 // На уровень выше (одну папку вверх)
