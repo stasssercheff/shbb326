3680ae549fc1ad479e17739e0e75978a496d5478
stasssercheff <stassserchef@gmail.com>
2026-01-09 09:04:24 +0700

    Update script.js

--- guest area/checklist/barista/script.js
+++ guest area/checklist/barista/script.js
@@ -1,1 +1,13 @@
+// На главную
+function goHome() {
+  // Переходим в корень сайта
+  location.href = location.origin + location.pathname.split('/')[1] + '/';
+}
 
+// На уровень выше (одну папку вверх)
+function goBack() {
+    const currentPath = window.location.pathname;
+    const parentPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
+    const upperPath = parentPath.substring(0, parentPath.lastIndexOf("/"));
+    window.location.href = upperPath + "/index.html";
+}
