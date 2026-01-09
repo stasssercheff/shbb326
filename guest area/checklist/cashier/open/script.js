8e1832cf7140690ff4d94d42e630a29c8fd20c9a
stasssercheff <stassserchef@gmail.com>
2026-01-09 09:07:39 +0700

    Update script.js

--- guest area/checklist/cashier/open/script.js
+++ guest area/checklist/cashier/open/script.js
@@ -19,6 +19,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+// На главную
+function goHome() {
+  // Переходим в корень сайта
+  location.href = location.origin + location.pathname.split('/')[1] + '/';
+}
+
+// На уровень выше (одну папку вверх)
+function goBack() {
+    const currentPath = window.location.pathname;
+    const parentPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
+    const upperPath = parentPath.substring(0, parentPath.lastIndexOf("/"));
+    window.location.href = upperPath + "/index.html";
+}
+
+  
   // Формируем сообщение на конкретном языке
   const buildMessage = (lang) => {
     const today = new Date();
