67835a6e316fb57c38397a88af9b68108adc760f
stasssercheff <stassserchef@gmail.com>
2026-01-09 08:50:41 +0700

    Update script-view.js

--- kitchen/schedule/script-view.js
+++ kitchen/schedule/script-view.js
@@ -1,6 +1,7 @@
 // На главную
 function goHome() {
-  location.href = "http://stasssercheff.github.io/shbb125/";
+  // Переходим в корень сайта
+  location.href = location.origin + location.pathname.split('/')[1] + '/';
 }
 
 // На уровень выше (одну папку вверх)
